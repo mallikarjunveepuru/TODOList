2472f668566051eb96294678e1ee67172c0664f4
R1cro <teseyko@outlook.com>
2016-03-26 18:55:35 +0300

    Users now can control his tasks. Task created status and close item options  added.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,9 @@ class ItemsController < ApplicationController
   before_action :show_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    if user_signed_in?
+    @items = Item.where(:user_id => current_user.id).order("created_at DESC")
+    end
   end
 
   def new
@@ -32,6 +34,12 @@ class ItemsController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
+    redirect_to root_path
   end
 
   def destroy
