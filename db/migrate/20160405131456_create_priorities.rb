710463a9d354402a83b3baed97d001cdcd6ac0a0
R1cro <teseyko@outlook.com>
2016-04-08 17:48:47 +0300

    Minor bug fixes.

--- db/migrate/20160405131456_create_priorities.rb
+++ db/migrate/20160405131456_create_priorities.rb
@@ -7,8 +7,8 @@ class CreatePriorities < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-      Priority.create  :name => "Low", :color => "#96F4A1", :priority_level => 3
-      Priority.create  :name => "Middle", :color => "#E9DC57", :priority_level => 2
-      Priority.create  :name => "High", :color => "#DF6060", :priority_level => 1
+    Priority.create  :name => "Low", :color => "#96F4A1", :priority_level => 3
+    Priority.create  :name => "Middle", :color => "#E9DC57", :priority_level => 2
+    Priority.create  :name => "High", :color => "#FFFFFF", :priority_level => 1
   end
 end
